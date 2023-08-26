462540204ded8626e7b1a33c0d8546b0a4bf566b
Shahjahan62 <shahjahanmukhtar62@gmail.com>
2023-08-26 10:57:11 +0500

    my first react app in material UI

---
+++ src/Components/NewComp.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@mui/material";
+import React from "react";
+
+export default function NewComp() {
+  return (
+    <div>
+      <Button variant="contained">hello world</Button>
+    </div>
+  );
+}
